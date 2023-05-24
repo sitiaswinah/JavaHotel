9b01bd8c9b65071ecb6683bba3c04c60cef1d9d6
sitiaswinah_ <130635724+sitiaswinah@users.noreply.github.com>
2023-05-24 20:55:23 +0700

    Add files via upload

---
+++ JavaHotel/src/main/java/com/pbo2/javahotel/HitungDiskon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbo2.javahotel;
+
+/**
+ *
+ * @author Win 10
+ */
+public class HitungDiskon {
+    public static void main(String[] args) {
+        TotalBayar totalbayar = new JavaHotel(){
+          
+        }
+                
+    }
+}
